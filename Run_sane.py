71cc428e08d6fbc28a8258ed334426d3aa637b92
Akreeeal <maksimfilipov1307@gmail.com>
2024-06-05 16:13:20 +0700

    Добавил модель нейронной сети

--- Run_sane.py
+++ Run_sane.py
@@ -1,10 +1,14 @@
 import numpy as np
-from generate_population import generate_population, n_epoches, num_blueprints, num_neuron_pop
+from generate_population import generate_population, n_epoches, num_blueprints, num_neuron_pop, num_input_neurons, num_hidden_neurons,num_output_neurons
 from load_data import X_train, y_train, X_val, y_val
+from model import Model
+from sklearn.metrics import log_loss
 
 def run_algorithm():
 
     epoch = 0
+    fitness_func = log_loss
+    lost_arr = []
 
     population, blueprints = generate_population()
 
@@ -13,6 +17,26 @@ def run_algorithm():
         blueprints_fitness = np.zeros(num_blueprints) # пригодности нейронной сети
         neuron_fitness = np.zeros(num_neuron_pop) # пригодности нейронов
         num_neuron_include = np.ones(num_neuron_pop) # пригодности вхождений нейронов в нейронную сеть
+        '''Пересчитать приспогодные комбинации'''
+        for bp_id in range(blueprints.shape(0)):
+            net = population[blueprints[bp_id]]
+            model = Model(net, num_input_neurons, num_output_neurons, num_hidden_neurons)
+            preds = model.forward(X_train, f='relu')
+            loss = fitness_func(y_train, preds)
+            blueprints_fitness[bp_id] = loss
+        '''сортировка модели от лучшей к худшей'''
+        sort_id = np.argsort(blueprints_fitness)
+        blueprints = blueprints[sort_id]
+        blueprints_fitness = blueprints_fitness[sort_id]
+        '''сохранение лучшей модели'''
+        best_loss_train = blueprints_fitness[0]
+        net = population[blueprints[0]]
+        model = Model(net, num_input_neurons, num_output_neurons, num_hidden_neurons)
+        preds = model.forward(X_val, f='relu')
+        best_loss_val = fitness_func(y_val,preds)
+        lost_arr.append(best_loss_val)
+
+
 
 
 
---
+++ model.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+class Model:
+    """
+    Класс модели однослойной ИНС
+    Аргументы:
+        - neural_network_neurons - параметры нейронной сети
+        - n_input_neurons - Количество входных нейронов
+        - n_output_neurons - Количество выходных нейронов
+        - n_hidden_neurons - количество нейронов скрытого слоя
+    """
+
+    def __init__(
+            self, neural_network_neurons,
+            n_input_neurons, n_output_neurons,
+            n_hidden_neurons
+    ):
+
+        # Количество нейронов входного слоя
+        self.n_input_neurons = n_input_neurons
+        # Количество нейронов выходного слоя
+        self.n_output_neurons = n_output_neurons
+        # Количество нейронов скрытого слоя
+        self.n_hidden_neurons = n_hidden_neurons
+
+        # Список нейронов входящих в нейронную сеть
+        self.neural_network_neurons = neural_network_neurons  # neuron_population[neuron_list]
+
+    def forward(self, x, f='relu'):
+        """
+        Функция для вычисления прямого
+        распространения сигнала по сети
+        """
+        if f == 'sigmoid':
+            act = self.Sigmoid
+        else:
+            act = self.ReLU
+
+        W1, W2 = self.create_weight_matrix()
+        return self.SoftMax(np.matmul(act(np.matmul(x, W1)), W2))
+
+    def ReLU(self, x):
+        """
+        Функция активации ReLU
+        """
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                if x[i, j] < 0:
+                    x[i, j] = 0
+        return x
+
+    def Sigmoid(self, x):
+        """
+        Функция активации ReLU
+        """
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                x[i, j] = 1 / (1 + np.exp(-x[i, j]))
+        return x
+
+    def SoftMax(self, x):
+        """
+        Функция активации SoftMax
+        """
+        out = np.zeros(x.shape)
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                out[i, j] = np.exp(x[i, j]) / np.sum(np.exp(x[i]))
+        return out
+
+    def backward(self, x, y, learning_rate=0.01):
+        """
+        Функция для обратного распространения ошибки
+        и обновления весов
+        """
+        # Прямое распространение
+        hidden_output = self.ReLU(np.matmul(x, self.W1))
+        final_output = self.SoftMax(np.matmul(hidden_output, self.W2))
+
+        # Ошибка на выходном слое
+        output_error = final_output - y
+
+        # Градиент на выходном слое
+        output_gradient = np.matmul(hidden_output.T, output_error)
+
+        # Обновление весов на выходном слое
+        self.W2 -= learning_rate * output_gradient
+
+        # Градиент на скрытом слое
+        hidden_error = np.matmul(output_error, self.W2.T)
+        hidden_gradient = np.matmul(x.T, hidden_error * (hidden_output > 0))
+
+        # Обновление весов на скрытом слое
+        self.W1 -= learning_rate * hidden_gradient
+
+    def create_weight_matrix(self):
+        """
+        Функция для формирования матриц весов
+        скрытого и выходного слоя
+        """
+        W1 = np.zeros((self.n_input_neurons, self.n_hidden_neurons))
+        W2 = np.zeros((self.n_hidden_neurons, self.n_output_neurons))
+
+        # Заполнение матрицы весов
+        for i in range(0, self.n_hidden_neurons):
+            hn_connections = self.neural_network_neurons[i]
+            for j in range(0, len(hn_connections), 2):
+                """
+                Если ID нейрона относится к входному слою 
+                (меньше количества нейронов во входном слое),
+                то заполняется матрицы весов скрытого слоя
+                """
+                if (hn_connections[j] < self.n_input_neurons):
+                    in_n_id = int(hn_connections[j])
+                    W1[in_n_id, i] = hn_connections[j + 1]
+                else:
+                    out_n_id = int(hn_connections[j] - self.n_input_neurons)
+                    W2[i, out_n_id] = hn_connections[j + 1]
+        return W1, W2
